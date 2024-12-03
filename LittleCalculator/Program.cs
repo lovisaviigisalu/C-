dcbfe8bf42616c8c1e67c8eed40281cb3977a9e3
lovisaviigisalu <122033147+lovisaviigisalu@users.noreply.github.com>
2024-12-03 11:38:28 +0200

    finish the calculator

--- LittleCalculator/Program.cs
+++ LittleCalculator/Program.cs
@@ -1,10 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter a whole number!");
+Console.WriteLine("Hi! Let's calculate the sum of two numbers!");
 
 //datatype variable name = value
 int myNumber = 0;
+int myNumber2 = 0;
 
+//taking the first user input and converting it to an integer
+Console.WriteLine("Enter the first (whole) number!");
 string userInput = Console.ReadLine();
 myNumber = int.Parse(userInput);
 
-Console.WriteLine("You entered: " + userInput);
+//taking the second user input and converting it to an integer
+Console.WriteLine("Enter the second (whole) number!");
+string userInput2 = Console.ReadLine();
+myNumber2 = int.Parse(userInput2);
+
+//calculating the sum and storing it in a variable
+int sum = myNumber + myNumber2;
+
+//printing the sum to the console
+Console.WriteLine("You entered: " + userInput + " and " + userInput2 + ". The sum is: " + sum);
+Console.ReadKey();
