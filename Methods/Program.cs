80a19cb7dc21f912c1eaf29b1c54311633b79524
lovisaviigisalu <122033147+lovisaviigisalu@users.noreply.github.com>
2024-12-15 14:32:55 +0200

    Intro to Functions /Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿// modifier returnType MethodName(Parameters){
+//     Code block
+// }
+void MyFirstMethod(){
+    Console.WriteLine("MyFirstMethod was called!");
+}
+//Calling the method
+MyFirstMethod();
+
+void writeSomething(string message){
+    Console.WriteLine(message);
+}
+string user = "Frank";
+//Calling the method
+writeSomething("Something to write");
+writeSomething(user);
+
+
+int AddTwoNumbers(int value1, int value2){
+    int sum = value1 + value2;
+    return sum;
+}
+Console.WriteLine("Write a number and i will add 10 to it.");
+int num1 = int.Parse(Console.ReadLine());
+int mySum = AddTwoNumbers(num1, 10);
+Console.WriteLine($"The sum is: {mySum}");
